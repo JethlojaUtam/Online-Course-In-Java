fdd45b6262e9277dd937c17172616516a141aeae
Jethloja Utam <utamjethloja@gmail.com>
2025-09-22 13:21:57 +0530

    Add files via upload

---
+++ CommandLineArgument2.java
@@ -0,0 +1,11 @@
+class CommandLineArgument2
+{
+public static void main(String[] args)
+{
+int i;
+for(i=0;i<args.length;i++)
+{
+System.out.println(Integer.parseInt(args[i]));		//this print is int
+}
+}
+}
